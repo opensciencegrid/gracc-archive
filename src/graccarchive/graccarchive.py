520da9204249ed01350f1a7b89c1a395d64e7490
Derek Weitzel <djw8605@gmail.com>
2017-05-24 11:01:56 -0500

    Removing None comparison

--- src/graccarchive/graccarchive.py
+++ src/graccarchive/graccarchive.py
@@ -168,7 +168,7 @@ class ArchiverAgent(object):
             os.fsync(fp.fileno())
         self.gzfile.flush()
         print "Cleared queue; ack'ing"
-        if self.delivery_tag != None:
+        if self.delivery_tag:
             self._chan.basic_ack(self.delivery_tag, multiple=True)
             self.delivery_tag = None
 
