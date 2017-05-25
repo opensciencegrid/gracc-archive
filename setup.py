3c184f801eca6f0076520cd01741aad69a64609e
Derek Weitzel <djw8605@gmail.com>
2017-05-25 13:55:38 -0500

    Bumping version and adding README

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 
 setup(name='graccarchive',
-      version='1.3.1',
+      version='1.4.0',
       description='GRACC Archive Agent',
       author_email='dweitzel@cse.unl.edu',
       author='Derek Weitzel',
