1cac6daeaf4191b0eb100f77f4b00d6be64d4505
Brian Bockelman <bbockelm@cse.unl.edu>
2017-05-25 17:18:28 -0500

    Remove unused imports.

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-from setuptools import setup, find_packages
-import os
-
+from setuptools import setup
 
 setup(name='graccarchive',
       version='1.4.0',
--- src/graccarchive/unarchive.py
+++ src/graccarchive/unarchive.py
@@ -13,12 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
 import tarfile
 import argparse
 import pika
-
-
 
 
 class UnArchiver(object):
