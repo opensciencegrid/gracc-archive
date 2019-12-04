08fc418e3cef89c71f2e4b8c643c72ffe22e026b
John Thiltges <jthiltges2@unl.edu>
2019-12-04 10:53:37 -0600

    Add options to filter replay by record date

--- src/graccarchive/unarchive.py
+++ src/graccarchive/unarchive.py
@@ -13,17 +13,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import tarfile
 import argparse
-import pika
+import datetime
 import json
+import tarfile
+import dateutil.parser
+import pika
 
 
 class UnArchiver(object):
 
-    def __init__(self, url, exchange):
+    def __init__(self, url, exchange, start_date=None, end_date=None):
         self.url = url
         self.exchange = exchange
+        self.start_date = start_date
+        self.end_date = end_date
         pass
 
     def createConnection(self):
@@ -35,10 +39,35 @@ class UnArchiver(object):
     def sendRecord(self, record):
         self._chan.basic_publish(exchange=self.exchange, routing_key='', body=record)
 
+    def dateFilter(self, record):
+        '''Filter records by date, returning false if record should be ignored'''
+        if not self.start_date and not self.end_date:
+            return True
+
+        json_record = json.loads(record)
+        try:
+            dt = None
+            # perfSonar: meta.ts_start = 1575331130
+            dt = datetime.datetime.utcfromtimestamp(json_record['meta']['ts_start'])
+            # GRACC: EndTime = "2019-12-02T22:38:58Z"
+            dt = datetime.datetime.strptime(json_record['EndTime'], '%Y-%m-%dT%H:%M:%SZ')
+        except KeyError:
+            pass
+        if dt is None:
+            # No timestamp found; allow the record
+            return True
+
+        if self.start_date and dt < self.start_date:
+            return False
+        if self.end_date and dt >= self.end_date:
+            return False
+        return True
+
     def parseTarFile(self, tar_file, start=0):
         tf = tarfile.open(tar_file, mode='r')
 
         counter = 0
+        sent_counter = 0
         # For each file in the tar file:
         for member in tf:
             if counter < start:
@@ -48,10 +77,13 @@ class UnArchiver(object):
                     tf.members = []
                 continue
             f = tf.extractfile(member)
-            self.sendRecord(f.read())
+            record = f.read()
+            if self.dateFilter(record):
+                self.sendRecord(record)
+                sent_counter += 1
             counter += 1
             if (counter % 10000) == 0:
-                print("Unpacked and sent {} records".format(counter))
+                print("Processed {} records and sent {} records".format(counter, sent_counter))
                 tf.members = []
 
         tf.close()
@@ -61,8 +93,8 @@ class PerfSonarUnArchiver(UnArchiver):
     """
     Subclass of the UnArchiver in order to send PS data
     """
-    def __init__(self, url, exchange):
-        super(PerfSonarUnArchiver, self).__init__(url, exchange)
+    def __init__(self, url, exchange, start_date=None, end_date=None):
+        super(PerfSonarUnArchiver, self).__init__(url, exchange, start_date, end_date)
 
     def sendRecord(self, record):
         # Parse the json record, looking for the "event-type"
@@ -84,13 +116,15 @@ def main():
     parser.add_argument("tarfile", nargs='+', help="Tar Files to parse and send")
     parser.add_argument("-p", "--psdata", help="Unarchive perfsonar data", action='store_true')
     parser.add_argument("-s", "--start", help="Record number to start sending", type=int, default=0)
+    parser.add_argument("--start-date", help="Select records on or after the specified date (UTC ISO-8601)", type=dateutil.parser.parse)
+    parser.add_argument("--end-date", help="Select records before the specified date (UTC ISO-8601)", type=dateutil.parser.parse)
 
     args = parser.parse_args()
 
     if args.psdata:
-        unarchive = PerfSonarUnArchiver(args.rabbiturl, args.exchange)
+        unarchive = PerfSonarUnArchiver(args.rabbiturl, args.exchange, args.start_date, args.end_date)
     else:
-        unarchive = UnArchiver(args.rabbiturl, args.exchange)
+        unarchive = UnArchiver(args.rabbiturl, args.exchange, args.start_date, args.end_date)
     unarchive.createConnection()
 
     for tar_file in args.tarfile:
